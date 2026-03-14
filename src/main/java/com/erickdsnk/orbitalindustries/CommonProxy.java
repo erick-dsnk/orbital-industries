bcf08c0fafe4feb33e8269f06b87d9c9595001d4
erick-dsnk <erick.8bld@gmail.com>
2026-03-14 22:33:48 +0100

    Refactor CommonProxy to enhance initialization process and logging. Integrate various registries and managers, including BlockRegistry, ItemRegistry, EntityRegistry, DimensionRegistry, PlanetRegistry, and environmental systems. Improve configuration loading with ConfigManager and add packet registration in init method.

--- src/main/java/com/erickdsnk/orbitalindustries/CommonProxy.java
+++ src/main/java/com/erickdsnk/orbitalindustries/CommonProxy.java
@@ -5,23 +5,70 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
+import com.erickdsnk.orbitalindustries.core.ConfigManager;
+import com.erickdsnk.orbitalindustries.core.OIModLogger;
+import com.erickdsnk.orbitalindustries.dimension.DimensionRegistry;
+import com.erickdsnk.orbitalindustries.environment.VacuumDamageHandler;
+import com.erickdsnk.orbitalindustries.environment.impl.OxygenSystemImpl;
+import com.erickdsnk.orbitalindustries.network.PacketHandler;
+import com.erickdsnk.orbitalindustries.planet.PlanetManager;
+import com.erickdsnk.orbitalindustries.planet.PlanetRegistry;
+import com.erickdsnk.orbitalindustries.registry.BlockRegistry;
+import com.erickdsnk.orbitalindustries.registry.EntityRegistry;
+import com.erickdsnk.orbitalindustries.registry.ItemRegistry;
+import com.erickdsnk.orbitalindustries.space.impl.AtmosphereManagerImpl;
+import com.erickdsnk.orbitalindustries.space.impl.GravityManagerImpl;
+import com.erickdsnk.orbitalindustries.space.impl.OrbitalEnvironmentManagerImpl;
+import com.erickdsnk.orbitalindustries.transport.LaunchManager;
+import com.erickdsnk.orbitalindustries.transport.TeleportManager;
+
 public class CommonProxy {
 
-    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
-    // GameRegistry." (Remove if not needed)
-    public void preInit(FMLPreInitializationEvent event) {
-        Config.synchronizeConfiguration(event.getSuggestedConfigurationFile());
+    private static final OIModLogger LOG = new OIModLogger("CommonProxy");
 
-        OrbitalIndustries.LOG.info(Config.greeting);
+    public void preInit(FMLPreInitializationEvent event) {
+        ConfigManager.load(event.getSuggestedConfigurationFile());
+        LOG.info("ConfigManager loaded");
+        OrbitalIndustries.LOG.info(ConfigManager.getGreeting());
         OrbitalIndustries.LOG.info("I am OrbitalIndustries at version " + Tags.VERSION);
+
+        BlockRegistry.registerBlocks();
+        LOG.info("BlockRegistry initialized");
+        ItemRegistry.registerItems();
+        LOG.info("ItemRegistry initialized");
+        EntityRegistry.registerEntities();
+        LOG.info("EntityRegistry initialized");
+
+        OrbitalIndustriesAPI.dimensionRegistry = new DimensionRegistry();
+        LOG.info("DimensionRegistry initialized");
+        OrbitalIndustriesAPI.planetRegistry = new PlanetRegistry();
+        LOG.info("PlanetRegistry initialized");
+        OrbitalIndustriesAPI.planetManager = new PlanetManager(OrbitalIndustriesAPI.planetRegistry);
+        LOG.info("PlanetManager initialized");
+        OrbitalIndustriesAPI.atmosphereManager = new AtmosphereManagerImpl(OrbitalIndustriesAPI.planetManager);
+        LOG.info("AtmosphereManager initialized");
+        OrbitalIndustriesAPI.gravityManager = new GravityManagerImpl(OrbitalIndustriesAPI.planetManager);
+        LOG.info("GravityManager initialized");
+        OrbitalIndustriesAPI.orbitalEnvironmentManager = new OrbitalEnvironmentManagerImpl();
+        LOG.info("OrbitalEnvironmentManager initialized");
+        OrbitalIndustriesAPI.launchManager = new LaunchManager();
+        LOG.info("LaunchManager initialized");
+        OrbitalIndustriesAPI.teleportManager = new TeleportManager();
+        LOG.info("TeleportManager initialized");
+        OrbitalIndustriesAPI.vacuumDamageHandler = new VacuumDamageHandler();
+        LOG.info("VacuumDamageHandler initialized");
+        OrbitalIndustriesAPI.oxygenSystem = new OxygenSystemImpl(OrbitalIndustriesAPI.atmosphereManager);
+        LOG.info("OxygenSystem initialized");
     }
 
-    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
-    public void init(FMLInitializationEvent event) {}
+    public void init(FMLInitializationEvent event) {
+        // TODO: Register concrete dimensions via DimensionRegistry when planets/dimensions are defined
+        LOG.info("DimensionRegistry ready");
+        PacketHandler.registerPackets();
+        LOG.info("PacketHandler ready");
+    }
 
-    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {}
 
-    // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {}
 }
