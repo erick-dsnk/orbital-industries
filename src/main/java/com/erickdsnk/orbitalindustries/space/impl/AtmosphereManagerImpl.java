a623580301d86072946ff380f570ced96d41f0f9
erick-dsnk <erick.8bld@gmail.com>
2026-03-14 22:31:03 +0100

    Add AtmosphereManagerImpl class to implement atmosphere checks for dimensions using PlanetManager. This class provides methods to determine if a dimension or planet has an atmosphere, supporting future environmental mechanics enhancements.

---
+++ src/main/java/com/erickdsnk/orbitalindustries/space/impl/AtmosphereManagerImpl.java
@@ -0,0 +1,30 @@
+package com.erickdsnk.orbitalindustries.space.impl;
+
+import com.erickdsnk.orbitalindustries.planet.Planet;
+import com.erickdsnk.orbitalindustries.planet.PlanetManager;
+import com.erickdsnk.orbitalindustries.space.AtmosphereManager;
+
+/**
+ * Default AtmosphereManager: looks up planet by dimension ID and returns hasAtmosphere.
+ *
+ * TODO: Space dimension (no planet) = no atmosphere. Overworld = breathable when not in space.
+ */
+public final class AtmosphereManagerImpl implements AtmosphereManager {
+
+    private final PlanetManager planetManager;
+
+    public AtmosphereManagerImpl(PlanetManager planetManager) {
+        this.planetManager = planetManager;
+    }
+
+    @Override
+    public boolean hasAtmosphere(int dimensionId) {
+        Planet planet = planetManager.getPlanetByDimensionId(dimensionId);
+        return planet != null && planet.hasAtmosphere();
+    }
+
+    @Override
+    public boolean hasAtmosphere(Planet planet) {
+        return planet != null && planet.hasAtmosphere();
+    }
+}
