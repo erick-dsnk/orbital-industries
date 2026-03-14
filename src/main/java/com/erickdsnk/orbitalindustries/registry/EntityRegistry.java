693b785e7b69c5bfb21e07b3e39a2acf54ce98ff
erick-dsnk <erick.8bld@gmail.com>
2026-03-14 22:32:40 +0100

    Add EntityRegistry class for central entity registration in the mod. This class includes a method for registering entities and outlines future plans for entity management, supporting the overall mod structure.

---
+++ src/main/java/com/erickdsnk/orbitalindustries/registry/EntityRegistry.java
@@ -0,0 +1,13 @@
+package com.erickdsnk.orbitalindustries.registry;
+
+/**
+ * Central registration for mod entities. Register with EntityRegistry (FML) during preInit.
+ *
+ * TODO: Register entity classes and spawn eggs; keep IDs in one place.
+ */
+public final class EntityRegistry {
+
+    public static void registerEntities() {
+        // TODO: EntityRegistry.registerModEntity(...) for rockets, etc.
+    }
+}
