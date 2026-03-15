ba2e349b3c64349d2d2e49cd59e533585b93e5a8
erick-dsnk <erick.8bld@gmail.com>
2026-03-15 14:01:10 +0100

    Add compileOnly dependency for Log4j API in build.gradle.kts and dependencies.gradle to ensure proper IDE resolution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,8 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+dependencies {
+    // Forge provides at runtime; needed for compile/IDE resolution of org.apache.logging.log4j
+    compileOnly("org.apache.logging.log4j:log4j-api:2.0.2")
+}
