99ec89dc7eca9a05ab28272f62ac11f047c40a57
erick-dsnk <erick.8bld@gmail.com>
2026-03-16 03:19:24 +0100

    Add LaunchState enum to represent various states of a rocket during its lifecycle, including IDLE, COUNTDOWN, LAUNCH, ASCENT, ORBIT, and LANDED.

---
+++ src/main/java/com/erickdsnk/orbitalindustries/rocket/LaunchState.java
@@ -0,0 +1,13 @@
+package com.erickdsnk.orbitalindustries.rocket;
+
+/**
+ * Launch state of a rocket entity.
+ */
+public enum LaunchState {
+    IDLE,
+    COUNTDOWN,
+    LAUNCH,
+    ASCENT,
+    ORBIT,
+    LANDED
+}
