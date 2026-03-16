9d7273817f900eacd4d0e85438a20517919e0904
erick-dsnk <erick.8bld@gmail.com>
2026-03-16 04:35:59 +0100

    Refactor PacketHandler to register AssembleRocketPacket for server communication. Cleaned up code formatting and removed TODO comments for improved clarity and maintainability.

--- src/main/java/com/erickdsnk/orbitalindustries/network/PacketHandler.java
+++ src/main/java/com/erickdsnk/orbitalindustries/network/PacketHandler.java
@@ -4,21 +4,21 @@ import com.erickdsnk.orbitalindustries.OrbitalIndustries;
 
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.relauncher.Side;
 
 /**
- * Registers Forge channel and packet types. Single entry point for network registration.
- *
- * TODO: Register concrete packet types (e.g. dimension sync, rocket state) with registerMessage.
+ * Registers Forge channel and packet types. Single entry point for network
+ * registration.
  */
 public final class PacketHandler {
 
     public static final SimpleNetworkWrapper CHANNEL = NetworkRegistry.INSTANCE
-        .newSimpleChannel(OrbitalIndustries.MODID);
+            .newSimpleChannel(OrbitalIndustries.MODID);
 
     private static int packetId = 0;
 
     public static void registerPackets() {
-        // TODO: CHANNEL.registerMessage(SomePacket.Handler.class, SomePacket.class, packetId++, Side.SERVER);
-        // TODO: CHANNEL.registerMessage(SomePacket.Handler.class, SomePacket.class, packetId++, Side.CLIENT);
+        CHANNEL.registerMessage(AssembleRocketPacket.Handler.class, AssembleRocketPacket.class, packetId++,
+                Side.SERVER);
     }
 }
