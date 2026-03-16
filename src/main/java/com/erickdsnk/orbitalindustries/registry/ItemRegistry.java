09c43181293d0c8b8ca9e01edc2993889cba548d
erick-dsnk <erick.8bld@gmail.com>
2026-03-16 04:33:53 +0100

    Implement item registration for rocket parts and rockets in ItemRegistry. This update includes instantiation and registration of ItemRocket and ItemRocketPart with GameRegistry, enhancing the mod's item management capabilities.

--- src/main/java/com/erickdsnk/orbitalindustries/registry/ItemRegistry.java
+++ src/main/java/com/erickdsnk/orbitalindustries/registry/ItemRegistry.java
@@ -1,13 +1,23 @@
 package com.erickdsnk.orbitalindustries.registry;
 
+import com.erickdsnk.orbitalindustries.item.rocket.ItemRocket;
+import com.erickdsnk.orbitalindustries.item.rocket.ItemRocketPart;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+
 /**
- * Central registration for mod items. Register with GameRegistry during preInit.
- *
- * TODO: Create and register items (e.g. rocket parts, oxygen tanks); keep names in one place.
+ * Central registration for mod items. Register with GameRegistry during
+ * preInit.
  */
 public final class ItemRegistry {
 
+    public static ItemRocketPart itemRocketPart;
+    public static ItemRocket itemRocket;
+
     public static void registerItems() {
-        // TODO: Instantiate items and GameRegistry.registerItem(...)
+        itemRocketPart = new ItemRocketPart();
+        GameRegistry.registerItem(itemRocketPart, "rocket_part");
+        itemRocket = new ItemRocket();
+        GameRegistry.registerItem(itemRocket, "rocket");
     }
 }
