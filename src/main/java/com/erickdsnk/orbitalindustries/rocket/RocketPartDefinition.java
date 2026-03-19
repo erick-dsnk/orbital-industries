58f2e473b6d66dfacfd63bd29ab130e25fd0d86e
erick-dsnk <erick.8bld@gmail.com>
2026-03-20 00:46:37 +0100

    Add RocketPartDefinition class to model rocket parts from JSON. This new data model includes fields for id, type, thrust, fuel capacity, mass, navigation tier, and max range, along with corresponding getters and setters for each attribute.

---
+++ src/main/java/com/erickdsnk/orbitalindustries/rocket/RocketPartDefinition.java
@@ -0,0 +1,87 @@
+package com.erickdsnk.orbitalindustries.rocket;
+
+/**
+ * Data model for a rocket part loaded from JSON. Omitted JSON fields
+ * deserialize
+ * as zero/false defaults via Gson (primitives).
+ */
+public final class RocketPartDefinition {
+
+    private String id;
+    private RocketPartType type;
+    private double thrust;
+    private double fuelCapacity;
+    private double mass;
+    private int navigationTier;
+    private double maxRange;
+
+    public RocketPartDefinition() {
+    }
+
+    public RocketPartDefinition(String id, RocketPartType type, double thrust,
+            double fuelCapacity, double mass, int navigationTier, double maxRange) {
+        this.id = id;
+        this.type = type;
+        this.thrust = thrust;
+        this.fuelCapacity = fuelCapacity;
+        this.mass = mass;
+        this.navigationTier = navigationTier;
+        this.maxRange = maxRange;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public RocketPartType getType() {
+        return type;
+    }
+
+    public void setType(RocketPartType type) {
+        this.type = type;
+    }
+
+    public double getThrust() {
+        return thrust;
+    }
+
+    public void setThrust(double thrust) {
+        this.thrust = thrust;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public int getNavigationTier() {
+        return navigationTier;
+    }
+
+    public void setNavigationTier(int navigationTier) {
+        this.navigationTier = navigationTier;
+    }
+
+    public double getMaxRange() {
+        return maxRange;
+    }
+
+    public void setMaxRange(double maxRange) {
+        this.maxRange = maxRange;
+    }
+}
